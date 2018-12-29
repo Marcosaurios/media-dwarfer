f239cd577afea5bdd8e9593245445084d0b0c0da
Marcos <marcosuriosgomez@gmail.com>
2018-12-29 12:24:33 +0100

    Getting video's FPS working :) using static library ffprobe

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 
 
-const Glob = require("glob").Glob;
-const ffmpeg = require('fluent-ffmpeg');
+var Glob = require("glob").Glob;
 
-var file_list;
+var ffprobe = require('ffprobe');
+var ffprobeStatic = require('ffprobe-static');
+
+var basename = "D:/Videos/";
+var file_list = [];
+
 /* Reading settings */
 
+/*
+60000/1000 = 60 fps
+60000/1001 = 59.94fps
+*/
 
 /* Listing files */
 
@@ -13,32 +21,34 @@ console.log("Finding .mp4 and .MP4 files on D:/Videos...")
 var found = new Glob(
     "**/*.{mp4,MP4}",
     {
-        cwd: "D:/Videos",
+        cwd: basename,
         matchBase:true
     },
     function (err, files) {
         console.log("... found: ")
         files.forEach(element => {
             console.log(element);
+            file_list.push(basename+element)
         });
         
-        file_list = files;
-        //console.log(files)
         
-        console.log("-------")
+        console.log()        
         console.log(file_list)
-        console.log("-------")
-        
-        ffmpeg.ffprobe(file_list, (error, meta) => {
-            console.log(meta)
+        console.log()
+    
+
+        console.log()        
+        console.log("file list [0]:" + files[0])
+        console.log()
+    
+        console.log("ffprobe: ...")    
+        ffprobe( file_list[0] , { path: ffprobeStatic.path }, function (err, info) {
+            if (err) console.log(err);
+            console.log(info.streams[0].r_frame_rate);
         })
+
+        
 })
-
-
-// ffmpeg.ffprobe(file_list, (error, metadata) => {
-//   const duration = metadata.format.duration;
-//   console.log(duration);
-// });
 
 /* Compressing by its quality */
 
